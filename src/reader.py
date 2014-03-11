33675d5318333cb599628e4cefebe93a2ca61a90
zqqf16 <zqqf16@gmail.com>
2014-03-11 18:08:38 +0800

    working on readers

--- src/reader.py
+++ src/reader.py
@@ -7,14 +7,12 @@ import markdown
 
 from datetime import datetime
 
-ALL_METAS = ['title', 'date', 'tags', 'publish', 'top']
-
 class AutoRegister(type):
     def __new__(self, name, bases, attrs):
         cls = type.__new__(self, name, bases, attrs)
 
         for base in bases:
-            # register 
+            # call register function of base class.
             if hasattr(base, 'register'):
                 base.register(cls)
 
@@ -28,14 +26,18 @@ class Reader(object):
         file_extensions: List, all supporting file types.
     '''
 
-    readers = {}
-
     file_extensions = []
+
+    # All supported metadata
+    metadata = ['title', 'date', 'tags', 'publish', 'top']
+
+    # Subclasses will be registered to this dict.
+    _readers = {}
 
     @classmethod
     def register(cls, subclass):
         for f in subclass.file_extensions:
-            cls.readers[f] = subclass
+            cls._readers[f] = subclass
 
     def read(self, file_path):
         '''Read draft from path
@@ -49,7 +51,7 @@ class Reader(object):
 class MarkdownReader(Reader):
     '''Markdown reader'''
 
-    file_extensions = ['md', 'markdown']
+    file_extensions = ['md', 'markdown'] #markdown type
 
     file_name_regex = re.compile(r'([^/]+)\.(md|markdown)')
 
@@ -67,15 +69,15 @@ class MarkdownReader(Reader):
                       'nl2br',        # New Line to Break
                       ]
 
-        # Do not guess language
+        # Do not guess the code language
         extension_configs = {'codehilite': [('guess_lang', False)]}
 
         self.md = markdown.Markdown(extensions=extensions,
                                     extension_configs=extension_configs)
- 
+
     _meta_handlers = [lambda x: x[0] if x else '', #title
                       lambda x: datetime.strptime(x[0], '%Y-%m-%d') if x else datetime.now(), #date
-                      lambda x: x[0].split(',') if x else [], #tag 
+                      lambda x: x[0].split(',') if x else [], #tag
                       lambda x: False if x and x[0]=='no' else True, #publish defaults to True
                       lambda x: True if x and x[0]=='yes' else False, #top defaults to False
                       ]
@@ -91,7 +93,7 @@ class MarkdownReader(Reader):
         '''
 
         res = {}
-        handlers = dict(zip(ALL_METAS, self._meta_handlers))
+        handlers = dict(zip(self.metadata, self._meta_handlers))
         for name, handler in handlers.items():
             value = handler(meta.get(name, None))
             res[name] = value
@@ -99,25 +101,25 @@ class MarkdownReader(Reader):
         return res
 
     def _parse_slug(self, file_name):
-        '''parse slug from file name'''
+        '''Parse slug from file name.'''
 
         m = self.file_name_regex.search(file_name)
         if not m:
             return None
         return m.group(1)
 
-    def read(self, file_name):
+    def read(self, file_path):
         '''Read markdown file.'''
 
-        slug = self._parse_slug(file_name)
+        slug = self._parse_slug(file_path)
         draft = {}
 
-        with open(file_name, 'r') as f:
+        with open(file_path, 'r') as f:
             content = f.read().decode('utf-8')
             html = self.md.reset().convert(content.strip(' \n'))
             if self.md.Meta:
                 draft = self._parse_metadata(self.md.Meta)
-            
+
             draft['content'] = html
             draft['slug'] = slug
             return draft
