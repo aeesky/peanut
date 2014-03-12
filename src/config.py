6b1f2783ac6974074fd53d9565d793662ec498bb
zqqf16 <zqqf16@gmail.com>
2014-03-12 23:05:28 +0800

    add config

---
+++ src/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+blog = {
+    'domain': 'blog.zorro.im',
+    'title': u'穷折腾',
+    'description': u'Zorro的个人博客',
+    'sitemap': True,
+    'rss': False,
+}
+
+path = {
+    'drafts' = 'drafts',
+    'html' = 'html',
+    'static' = 'static',
+}
+
+url = {
+    'post' = 'post/{slug}.html',
+    'tag' = 'tag/{slug}.html',
+    'index' = 'index.html',
+    'sitemap' = 'sitemap.xml',
+    'rss' = 'rss.xml',
+}
